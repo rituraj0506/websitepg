dd69ce349791d321627f21ab92da027abdc92ce8
rituraj0506 <riturajkumar7256@gmail.com>
2025-07-22 22:28:10 +0530

    added changes

--- lib/model/gallery.dart
+++ lib/model/gallery.dart
@@ -11,7 +11,7 @@ final List<GalleryImage> galleryImages = [
   GalleryImage(imagePath: 'asset/basketball.png', caption: 'Basket Ball'),
   GalleryImage(imagePath: 'asset/library.png', caption: 'Library'),
   GalleryImage(imagePath: 'asset/purisabji.jpg', caption: 'Healthy Food'),
-  GalleryImage(imagePath: 'asset/lunch.jpg', caption: 'Healthy Food'),
+  GalleryImage(imagePath: 'asset/friedrice.jpg', caption: 'Healthy Food'),
   GalleryImage(imagePath: 'asset/cafeteria.png', caption: 'CafeTeria'),
   GalleryImage(imagePath: 'asset/ro.jpg', caption: 'RO Water'),
   GalleryImage(imagePath: 'asset/wifi.jpg', caption: 'Wifi'),
